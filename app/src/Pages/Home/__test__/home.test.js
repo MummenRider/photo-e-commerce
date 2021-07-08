6283830d47b81004ab8d99a3657bd701b593fff7
Clark Inocalla <43891568+MummenRider@users.noreply.github.com>
2021-07-08 17:53:51 -0600

    added snapshot to uI components (#42)

--- app/src/Pages/Home/__test__/home.test.js
+++ app/src/Pages/Home/__test__/home.test.js
@@ -5,8 +5,17 @@ import HeroSection from "Components/Hero/hero";
 afterEach(cleanup);
 
 describe("hero component in home page", () => {
-  test("successfully rendered", () => {
-    render(<HeroSection />);
-    expect(screen.getByTestId("hero-container")).toBeInTheDocument();
+  beforeEach(() => render(<HeroSection />));
+
+  it("should have an image ", () => {
+    expect(screen.getByAltText(/image-background/i)).toBeInTheDocument();
+  });
+
+  it("should have a header photography", () => {
+    expect(screen.getByText(/photography/i)).toBeInTheDocument();
+  });
+
+  it("should have explore line ", () => {
+    expect(screen.getByTestId("explore-line")).toBeInTheDocument();
   });
 });
