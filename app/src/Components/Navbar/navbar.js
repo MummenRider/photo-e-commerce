e397783fa3e3cf94d47481a4ea8cebff03422d41
Clark Inocalla <43891568+MummenRider@users.noreply.github.com>
2021-07-13 14:10:39 -0600

    Completed nav on scroll effect (#46)

--- app/src/Components/Navbar/navbar.js
+++ app/src/Components/Navbar/navbar.js
@@ -1,29 +1,32 @@
 import React, { useState } from "react";
+import { InView } from "react-intersection-observer";
 import { Nav, Logo, Hamburger, Menu, MenuLink, Header } from "./navbar.styles";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [isScrolled, setIsScrolled] = useState(false);
   return (
-    <Header>
-      <Nav data-testid="nav">
-        <Logo data-testid="logo" to="/">
-          <h3>LOGO</h3>
-        </Logo>
-        <Hamburger onClick={() => setIsOpen(!isOpen)}>
-          <span />
-          <span />
-          <span />
-        </Hamburger>
-        <Menu isOpen={isOpen}>
-          <MenuLink to="/about" data-testid="about">
-            ABOUT
-          </MenuLink>
-          <MenuLink to="/services">SERVICES</MenuLink>
-          <MenuLink to="/contact">CONTACT</MenuLink>
-        </Menu>
-      </Nav>
-    </Header>
+    <InView as="div" onChange={setIsScrolled}>
+      <Header isScrolled={isScrolled}>
+        <Nav data-testid="nav">
+          <Logo data-testid="logo" to="/">
+            <h3>LOGO</h3>
+          </Logo>
+          <Hamburger onClick={() => setIsOpen(!isOpen)}>
+            <span />
+            <span />
+            <span />
+          </Hamburger>
+          <Menu isOpen={isOpen}>
+            <MenuLink to="/about" data-testid="about">
+              ABOUT
+            </MenuLink>
+            <MenuLink to="/services">SERVICES</MenuLink>
+            <MenuLink to="/contact">CONTACT</MenuLink>
+          </Menu>
+        </Nav>
+      </Header>
+    </InView>
   );
 };
 
