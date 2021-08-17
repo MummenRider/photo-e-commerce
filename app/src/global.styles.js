7ebe844f2adaeaeabe0b033e0e6d264cae11b9b7
Clark Inocalla <43891568+MummenRider@users.noreply.github.com>
2021-08-17 16:10:02 -0600

    Customized the look of side scrollbar (#80)

--- app/src/global.styles.js
+++ app/src/global.styles.js
@@ -7,10 +7,12 @@ export const GlobalStyles = createGlobalStyle`
   --ff-secondary: lust-sans, sans-serif;
   --bg-primary: #111112;
   --bg-secondary: #151515;
+  scrollbar-width: thin;
 }
 
 html {
   font-size: 62.5%;
+  scroll-behavior:smooth;
 }
 
 * {
@@ -23,6 +25,21 @@ html {
 body {
   background: var(--bg-primary);
   overflow-x: hidden;
+}
+
+body::-webkit-scrollbar {
+  width: 1em;
+}
+
+body::-webkit-scrollbar-thumb {
+  background-color:rgb(117 114 114);
+  border-radius:25px;
+
+}
+
+body::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+ 
 }
 `;
 
