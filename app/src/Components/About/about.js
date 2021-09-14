29e15ae643400f737de97ecb7f76bedcc7ccdfdb
Clark Inocalla <43891568+MummenRider@users.noreply.github.com>
2021-09-14 10:27:42 -0600

    About section effect (#88)
    * fixed the styling when zooming
    
    * Added effect on Image and text
    
    * updated tests in about.test.js

--- app/src/Components/About/about.js
+++ app/src/Components/About/about.js
@@ -7,7 +7,10 @@ import {
   Description,
   Inner,
   Frames,
+  Left,
+  Right,
 } from "Components/About/about.styles";
+import { useInView } from "react-intersection-observer";
 
 export default function About({ children, ...restProps }) {
   return (
@@ -18,9 +21,22 @@ export default function About({ children, ...restProps }) {
 }
 
 About.Image = function AboutImage({ src, ...restProps }) {
+  const [ref, inView] = useInView({
+    rootMargin: "0px 0px -30% 0px",
+  });
   return (
     <Frames {...restProps}>
-      <Image url={src} />
+      <Image
+        url={src}
+        ref={ref}
+        animate={{
+          scale: inView ? 1.0 : 1.1,
+        }}
+        transition={{
+          ease: [0.43, 0.13, 0.23, 0.96],
+          duration: 0.8,
+        }}
+      />
     </Frames>
   );
 };
@@ -29,10 +45,39 @@ About.Content = function AboutContent({ children, ...restProps }) {
   return <Content {...restProps}>{children}</Content>;
 };
 
+About.Left = function AboutLeft({ children, ...restProps }) {
+  return <Left {...restProps}>{children}</Left>;
+};
+
+About.Right = function AboutRight({ children, ...restProps }) {
+  return <Right {...restProps}>{children}</Right>;
+};
+
 About.Title = function AboutTitle({ isBigDevice, children, ...restProps }) {
   return isBigDevice ? <Title {...restProps}>{children}</Title> : null;
 };
 
-About.Description = function AboutDescription({ children, ...restProps }) {
-  return <Description {...restProps}>{children}</Description>;
+About.Description = function AboutDescription({
+  options,
+  children,
+  ...restProps
+}) {
+  const [ref, inView] = useInView({ ...options });
+
+  return (
+    <Description
+      ref={ref}
+      {...restProps}
+      animate={{
+        y: inView ? 0 : 100,
+        opacity: inView ? [0.2, 0.4, 0.8, 1] : 0,
+      }}
+      transition={{
+        ease: [0.43, 0.13, 0.23, 0.96],
+        duration: 1,
+      }}
+    >
+      {children}
+    </Description>
+  );
 };
