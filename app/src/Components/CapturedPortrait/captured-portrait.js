3974be1832066ce48768f3d6dfd86f7ca458fcec
Clark Inocalla <43891568+MummenRider@users.noreply.github.com>
2021-11-02 13:13:41 -0600

    completed applying effect on the images. Next commit will be the text animation

--- app/src/Components/CapturedPortrait/captured-portrait.js
+++ app/src/Components/CapturedPortrait/captured-portrait.js
@@ -6,42 +6,53 @@ import {
   SubTitle,
   Right,
 } from "Components/CapturedPortrait/captured-portrait.styles";
-
+import { useInView } from "react-intersection-observer";
 export default function CapturedPortrait({ children, ...restProps }) {
   return <Container {...restProps}>{children}</Container>;
 }
-
 CapturedPortrait.Block = function CapturedPortraitBlock({
   children,
   ...restProps
 }) {
   return <Block {...restProps}>{children}</Block>;
 };
-
 CapturedPortrait.Left = function CapturedPortraitLeft({
   children,
   ...restProps
 }) {
   return <Left {...restProps}>{children}</Left>;
 };
-
 CapturedPortrait.SubTitle = function CapturedPortraitSubTitle({
   children,
   ...restProps
 }) {
   return <SubTitle {...restProps}>{children}</SubTitle>;
 };
-
 CapturedPortrait.MainTitle = function CapturedPortraitMainTitle({
   children,
   ...restProps
 }) {
   return <MainTitle {...restProps}>{children}</MainTitle>;
 };
-
 CapturedPortrait.Right = function CapturedPortraitRight({
   children,
   ...restProps
 }) {
-  return <Right {...restProps}>{children}</Right>;
+  const [ref, inView] = useInView({ rootMargin: "120% 0px 0px 0px" });
+  return (
+    <Right
+      {...restProps}
+      style={{ transformOrigin: "top center" }}
+      transition={{
+        ease: [0.43, 0.13, 0.23, 0.96],
+        duration: 0.8,
+      }}
+      animate={{
+        scale: inView ? 1 : 1.1,
+      }}
+      ref={ref}
+    >
+      {children}
+    </Right>
+  );
 };
