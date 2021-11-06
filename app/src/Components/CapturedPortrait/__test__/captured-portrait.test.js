85c75225b4cdba5773bc26cfbe107802492edbe1
Clark Inocalla <43891568+MummenRider@users.noreply.github.com>
2021-11-06 10:28:11 -0600

    Captured portrait effect (#91)
    * completed applying effect on the images. Next commit will be the text animation
    
    * Text effect has been added. Next commit will be updated tests
    
    * completed updating tests for captured portraits component

--- app/src/Components/CapturedPortrait/__test__/captured-portrait.test.js
+++ app/src/Components/CapturedPortrait/__test__/captured-portrait.test.js
@@ -2,6 +2,8 @@ import { getByAltText, render } from "@testing-library/react";
 import CapturedPortrait from "Components/CapturedPortrait/captured-portrait";
 import { capturedImageData } from "Components/CapturedPortrait/portrait-data";
 import LoadImage from "Components/ImageBlock/image-block";
+import { useInView } from "react-intersection-observer";
+import { mockAllIsIntersecting } from "react-intersection-observer/test-utils";
 
 describe("<CapturedPortrait />", () => {
   it("should render <CapturedPortrait /> with populated data", () => {
@@ -10,15 +12,17 @@ describe("<CapturedPortrait />", () => {
         {capturedImageData.map((imageData) => (
           <CapturedPortrait.Block key={imageData.id}>
             <CapturedPortrait.Left>
-              <CapturedPortrait.SubTitle>
-                {imageData.topSubTitle}
-              </CapturedPortrait.SubTitle>
-              <CapturedPortrait.MainTitle>
-                {imageData.mainTitle}
-              </CapturedPortrait.MainTitle>
-              <CapturedPortrait.SubTitle>
-                {imageData.bottomSubTitle}
-              </CapturedPortrait.SubTitle>
+              <CapturedPortrait.TextContainer>
+                <CapturedPortrait.SubTitle>
+                  {imageData.topSubTitle}
+                </CapturedPortrait.SubTitle>
+                <CapturedPortrait.MainTitle>
+                  {imageData.mainTitle}
+                </CapturedPortrait.MainTitle>
+                <CapturedPortrait.SubTitle>
+                  {imageData.bottomSubTitle}
+                </CapturedPortrait.SubTitle>
+              </CapturedPortrait.TextContainer>
             </CapturedPortrait.Left>
             <CapturedPortrait.Right>
               <LoadImage
@@ -31,6 +35,7 @@ describe("<CapturedPortrait />", () => {
         ))}
       </CapturedPortrait>
     );
+    mockAllIsIntersecting(true);
 
     expect(getAllByText("LOREM, IPSUM, NONUMY")).toBeTruthy();
     expect(getAllByText("Lorem Ipsum Dolor Amet Conseteur")).toBeTruthy();
@@ -39,5 +44,22 @@ describe("<CapturedPortrait />", () => {
     expect(getByAltText("Ariana image")).toBeTruthy();
     expect(getByAltText("Liz image")).toBeTruthy();
     expect(getByAltText("Bailey image")).toBeTruthy();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+  const CapturedPortraitMock = ({ options }) => {
+    const [ref, inView] = useInView({ ...options });
+    return <div ref={ref}>{inView.toString()}</div>;
+  };
+  it("should render element when in view", () => {
+    const { getByText } = render(
+      <CapturedPortraitMock options={{ threshold: 0.5 }}>
+        Hi
+      </CapturedPortraitMock>
+    );
+    mockAllIsIntersecting(0.1);
+    expect(getByText("false")).toBeTruthy();
+
+    mockAllIsIntersecting(0.5);
+    expect(getByText("true")).toBeTruthy();
   });
 });
--- app/src/Components/CapturedPortrait/captured-portrait.styles.js
+++ app/src/Components/CapturedPortrait/captured-portrait.styles.js
@@ -1,19 +1,16 @@
 import styled from "styled-components";
-
+import { motion } from "framer-motion";
 export const Container = styled.section`
   color: white;
   display: flex;
   flex-direction: column;
 `;
-
 export const Block = styled.div`
   display: flex;
-
   @media screen and (max-width: 768px) {
     flex-direction: column-reverse;
   }
 `;
-
 export const Left = styled.div`
   height: auto;
   width: 100%;
@@ -29,13 +26,19 @@ export const Left = styled.div`
   }
 `;
 
-export const SubTitle = styled.span`
-  font-family: var(--ff-primary);
-  font-size: clamp(1.2rem, 1.2vw, 8rem);
-  font-weight: 100;
-  padding: 4em 0;
+export const TextContainer = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 76%;
 `;
-
+export const SubTitle = styled(motion.span)`
+  font-family: var(--ff-primary);
+  font-size: clamp(1rem, 0.7vw, 8rem);
+  font-weight: 100;
+  padding: 5em 0;
+`;
 export const MainTitle = styled.h1`
   font-family: var(--ff-secondary);
   font-size: clamp(2.2rem, 2.5vw, 16rem);
@@ -45,14 +48,12 @@ export const MainTitle = styled.h1`
   letter-spacing: 2px;
   padding: 0 1em;
   width: 80%;
-
   @media screen and (min-width: 1024px) {
     padding: 0 3em;
     width: 100%;
   }
 `;
-
-export const Right = styled.div`
+export const Right = styled(motion.div)`
   height: auto;
   width: 100%;
   @media screen and (min-width: 1024px) {
