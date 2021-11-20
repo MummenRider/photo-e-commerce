c197e7bf69d17054f109c6432ab73d6c200a8219
MummenRider <clark.inocalla@hotmail.com>
2021-11-19 22:42:31 -0600

    completed adding effects in abstract photo component

--- app/src/Components/AbstractPhoto/abstract-photo.styles.js
+++ app/src/Components/AbstractPhoto/abstract-photo.styles.js
@@ -1,8 +1,10 @@
-import styled from "styled-components";
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
 
 export const Container = styled.section`
   margin-top: 50%;
   background-color: #dbd8c7;
+
   display: flex;
   flex-direction: column;
 
@@ -29,7 +31,7 @@ export const CenterBlock = styled.div`
   }
 `;
 
-export const RightBlock = styled.div`
+export const RightBlock = styled(motion.div)`
   position: relative;
   width: 100%;
 
@@ -48,27 +50,29 @@ export const ImageItem = styled.div`
 `;
 
 export const Title = styled.h1`
-  font-family: var(--ff-secondary);
+  /* font-family: var(--ff-secondary); */
+  font-family: broadacre-thin-1, sans-serif;
   font-size: 4rem;
   font-weight: 200;
   text-align: center;
 
   @media screen and (min-width: 768px) {
-    font-size: clamp(3rem, 2.5vw, 16rem);
+    font-size: clamp(3rem, 2.8vw, 16rem);
+    font-weight: 100;
   }
 `;
 
 export const Description = styled.p`
   font-family: var(--ff-primary);
   font-size: 1.2rem;
-  font-weight: 200;
-  line-height: 1.6;
-  letter-spacing: 1px;
+  font-weight: 300;
+  line-height: 1.8;
+  letter-spacing: 1.1px;
   text-align: center;
   padding: 5% 10% 10% 10%;
 
   @media screen and (min-width: 768px) {
-    font-size: clamp(0.8rem, 0.9vw, 6rem);
+    font-size: clamp(0.8rem, 0.6vw, 6rem);
   }
 
   @media screen and (min-width: 1100px) {
@@ -76,7 +80,8 @@ export const Description = styled.p`
   }
 
   @media screen and (min-width: 1100px) {
-    padding: 10% 15%;
+    width: 70%;
+    align-self: center;
   }
 `;
 export const Divider = styled.div`
@@ -84,20 +89,20 @@ export const Divider = styled.div`
   left: 30%;
 
   margin: 20% 0 15% 0;
-  min-height: 0.1%;
+  min-height: 0.03%;
   width: 40%;
-  background-color: #949494;
+  background-color: #5b5b5b;
   transform: rotate(-45deg);
 `;
 
-export const ImageBlockOne = styled.div`
+export const ImageBlockOne = styled(motion.div)`
   position: absolute;
   top: -42%;
   right: 0%;
   width: 12vw;
 `;
-
-export const ImageBlockTwo = styled.div`
+export const ImageBlockTwoInner = styled(motion.div)``;
+export const ImageBlockTwo = styled(motion.div)`
   position: relative;
   top: -20%;
   left: 50%;
@@ -113,7 +118,7 @@ export const ImageBlockTwo = styled.div`
   }
 `;
 
-export const ImageBlockThree = styled.div`
+export const ImageBlockThree = styled(motion.div)`
   position: absolute;
   right: 0;
   bottom: -60%;
